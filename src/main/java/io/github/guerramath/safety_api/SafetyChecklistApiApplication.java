32abc0e593ed7a3638dbb08af0ea99b25214e915
Matheus Guerra <ten.matheus.guerra@gmail.com>
2026-01-25 00:35:46 -0300

    Build: Estabilização completa do sistema, Swagger UI e PostgreSQL

--- src/main/java/io/github/guerramath/safety_api/SafetyChecklistApiApplication.java
+++ src/main/java/io/github/guerramath/safety_api/SafetyChecklistApiApplication.java
@@ -1,13 +1,21 @@
 package io.github.guerramath.safety_api;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Safety Checklist API",
+				version = "1.0",
+				description = "Gerenciamento de Risco Operacional - Cmte. Matheus Guerra"
+		)
+)
 public class SafetyChecklistApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SafetyChecklistApiApplication.class, args);
 	}
-
 }
--- src/main/java/io/github/guerramath/safety_api/controller/GlobalExceptionHandler.java
+++ src/main/java/io/github/guerramath/safety_api/controller/GlobalExceptionHandler.java
@@ -10,22 +10,17 @@ import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-@ControllerAdvice // <--- Isso diz ao Spring: "Olhe todos os erros de todos os Controllers"
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
-    // Intercepta o erro que lançamos no Service quando a mitigação falta
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<Object> handleSafetyException(RuntimeException ex, WebRequest request) {
-
-        // Montamos um JSON de resposta amigável
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("timestamp", LocalDateTime.now());
-        body.put("status", HttpStatus.BAD_REQUEST.value()); // Erro 400 (Erro do Cliente/Piloto)
-        body.put("error", "Bloqueio de Segurança Operacional");
-        body.put("message", ex.getMessage()); // A mensagem "ALERTA DE SEGURANÇA..." vem aqui
+        body.put("status", HttpStatus.BAD_REQUEST.value());
+        body.put("error", "Bloqueio de Seguranca Operacional");
+        body.put("message", ex.getMessage());
 
-
-        // Retorna HTTP 400 (Bad Request) em vez de 500
         return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 }
