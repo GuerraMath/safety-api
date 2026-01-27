b618e39490506f9ff6bfb6d8bfc223b6a67eb50c
Matheus Guerra <ten.matheus.guerra@gmail.com>
2026-01-27 10:42:55 -0300

    test: implement H2 in-memory database for isolated build tests

--- src/test/java/io/github/guerramath/safety_api/SafetyChecklistApiApplicationTests.java
+++ src/test/java/io/github/guerramath/safety_api/SafetyChecklistApiApplicationTests.java
@@ -2,12 +2,15 @@ package io.github.guerramath.safety_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // <--- A mágica acontece aqui
 class SafetyChecklistApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Se o contexto subir usando o H2, o teste passa!
 	}
 
 }
